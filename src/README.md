e71c2c6514912e7f5299ea6de47121b42665a01c
Adrian M. Popan <adrian.popan@gmail.com>
2021-04-22 19:12:24 -0500

    integrate routes, add Header, Progress, FirstStep

--- src/README.md
+++
@@ -1,1 +0,0 @@
-# multi_step_form_registration
---
+++ src/router/AppRouter/index.js
@@ -0,0 +1,18 @@
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import FirstStep from './../../components/FirstStep'
+import Header from './../../components/Header'
+
+const AppRouter = () => {
+  return (
+      <BrowserRouter>
+        <div className='container'>
+          <Header />
+          <Switch>
+            <Route component={FirstStep} path='/' exact={true} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+  )
+}
+
+export default AppRouter
