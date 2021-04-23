cdf7b03be9495a746a8fb8ddef2c47e660cbc017
Adrian M. Popan <adrian.popan@gmail.com>
2021-04-23 17:34:18 -0500

    add country, state, city selection functionality

--- src/components/ThirdStep/index.js
+++ src/components/ThirdStep/index.js
@@ -3,38 +3,160 @@ import { Form, Button } from 'react-bootstrap';
 import csc from 'country-state-city';
 import axios from 'axios';
 import { BASE_API_URL } from './../../utils';
+import { async } from 'q';
 
 const ThirdStep = () => {
-    const [countries, setCountries] = useState([]);
-    const [states, setStates] = useState([]);
-    const [cities, setCities] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
+  const [countries, setCountries] = useState([]);
+  const [states, setStates] = useState([]);
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-    const [selectedCountry, setSelectedCountry] = useState('');
-    const [selectedState, setSelectedState] = useState('');
-    const [selectedCity, setSelectedCity] = useState('');
+  const [selectedCountry, setSelectedCountry] = useState('');
+  const [selectedState, setSelectedState] = useState('');
+  const [selectedCity, setSelectedCity] = useState('');
 
-    useEffect(() => {
-        const getCountries = async () => {
-            try {
-                const result = await csc.getAllCountries();
-                console.log(result);
-            } catch (error) {
-                // console.log(error)
-            }
-        };
-        getCountries();
-    }, []);
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  useEffect(() => {
+    const getCountries = async () => {
+      try {
+        setIsLoading(true);
+        const result = await csc.getAllCountries();
+        let allCountries = [];
+        allCountries = result?.map(({ isoCode, name }) => ({
+          isoCode,
+          name,
+        }));
+        const [{ isoCode: firstCountry } = {}] = allCountries;
+        setCountries(allCountries);
+        setSelectedCountry(firstCountry);
+        setIsLoading(false);
+        // console.log(result);
+      } catch (error) {
+        setCountries([]);
+        setIsLoading(false);
+        // console.log(error)
+      }
     };
+    getCountries();
+  }, []);
 
-    return (
-        <Form className='input-form' onSubmit={handleSubmit}>
-            <div className='col-md-6 offset-md-3'></div>
-        </Form>
-    );
+  useEffect(() => {
+    const getStates = async () => {
+      try {
+        const result = await csc.getStatesOfCountry(selectedCountry);
+        let allStates = [];
+        allStates = result?.map(({ isoCode, name }) => ({
+          isoCode,
+          name,
+        }));
+        console.log({ allStates });
+        const [{ isoCode: firstState = '' } = {}] = allStates;
+        setCities([]);
+        setSelectedCity('');
+        setStates(allStates);
+        setSelectedState(firstState);
+      } catch (error) {
+        setStates([]);
+        setCities([]);
+        setSelectedCity('');
+        //   console.log(error)
+      }
+    };
+    getStates();
+  }, [selectedCountry]);
+
+  useEffect(() => {
+    const getCities = async () => {
+        try {
+            const result = await csc.getCitiesOfState(
+                selectedCountry,
+                selectedState
+            )
+            let allCities = []
+            allCities =result?.map(({ name }) => ({
+                name
+            }))
+            const [{ name: firstCity = ''} = {}] = allCities
+            setCities(allCities)
+            setSelectedCity(firstCity)
+        } catch (error) {
+            // console.log(error)   
+            setCities([])         
+        }
+    }
+    getCities()
+  }, [selectedState])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <Form className='input-form' onSubmit={handleSubmit}>
+      <div className='col-md-6 offset-md-3'>
+        <Form.Group controlId='country'>
+          {isLoading && (
+            <p className='loading'>Loading countries. Please wait...</p>
+          )}
+          <Form.Label>Country</Form.Label>
+          <Form.Control
+            as='select'
+            name='country'
+            value={selectedCountry}
+            onChange={(e) => setSelectedCountry(e.target.value)}
+          >
+            {countries.map(({ isoCode, name }) => (
+              <option value={isoCode} key={isoCode}>
+                {name}
+              </option>
+            ))}
+          </Form.Control>
+        </Form.Group>
+        <Form.Group controlId='state'>
+          <Form.Label>State / County</Form.Label>
+          <Form.Control
+            as='select'
+            name='state'
+            value={selectedState}
+            onChange={(e) => setSelectedState(e.target.value)}
+          >
+            {states.length > 0 ? (
+              states.map(({ isoCode, name }) => (
+                <option value={isoCode} key={isoCode}>
+                  {name}
+                </option>
+              ))
+            ) : (
+              <option value='' key=''>
+                No state found
+              </option>
+            )}
+          </Form.Control>
+        </Form.Group>
+        <Form.Group controlId='city'>
+            <Form.Label>City</Form.Label>
+            <Form.Control
+                as='select'
+                name='city'
+                value={selectedCity}
+                onChange={(e) => setSelectedCity(e.target.value)}
+            >
+                {cities.length > 0 ? (
+                    cities.map(({ name }) => (
+                        <option value={name} key={name}>
+                            {name}
+                        </option>
+                    ))
+                ) : (
+                    <option value=''>No cities found</option>
+                )}
+            </Form.Control>
+        </Form.Group>
+        <Button variant='primary' type='submit'>
+            Register
+        </Button>
+      </div>
+    </Form>
+  );
 };
 
 export default ThirdStep;
