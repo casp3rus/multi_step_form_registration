1fbcab7bfd27d835a33f660f1037ff72d59e8fcc
Adrian M. Popan <adrian.popan@gmail.com>
2021-04-23 19:43:30 -0500

    build backend server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const { response } = require('express')
+const express = require('express')
+require('./db')
+
+const app = express()
+const PORT = process.env.PORT || 3030
+
+app.get('/', (req, res) => {
+  res.send('<h2>This is from index.js file</h2>')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`)
+})
